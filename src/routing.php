9d96865a74c72ea147a268a47731928be2e4331c
Douglas Gonçalves <douglas.goncalves@grupofolha.com.br>
2020-07-11 14:36:18 -0300

    Atualização do driver

--- src/routing.php
+++ src/routing.php
@@ -5,10 +5,11 @@
     router::setInfoLocal(LOCAL_TYPE_REQUEST, LOCAL_ROOT, LOCAL_REQUEST);
 
     // ROTAS ADMIN
+    router::route('GET','/',"heartwood\\admin\\controllers\\productsUpdate");
     router::route('GET','/admin/products/update/:number',"heartwood\\admin\\controllers\\productsUpdate");
 
     // ROTAS API
     router::route('GET','/api/products/product/:number',"heartwood\\api\\controllers\\product");
 
-    exit();
+    exit(router::http_response_code(404));
 ?>
