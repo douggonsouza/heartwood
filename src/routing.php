0bf304219a1b5d361dc30d93d314adeb01c5771f
Douglas Gonçalves <douglas.goncalves@grupofolha.com.br>
2020-07-15 15:52:47 -0300

    Rotas com autenticação

--- src/routing.php
+++ src/routing.php
@@ -2,14 +2,19 @@
 
     use driver\router\router;
 
-    router::setInfoLocal(LOCAL_TYPE_REQUEST, LOCAL_ROOT, LOCAL_REQUEST);
+    router::setInfoLocal(PROTOCOL,LOCAL_TYPE_REQUEST, LOCAL_ROOT, LOCAL_REQUEST);
 
     // ROTAS ADMIN
     router::route('GET','/',"heartwood\\admin\\controllers\\productsUpdate");
     router::route('GET','/admin/products/update/:number',"heartwood\\admin\\controllers\\productsUpdate");
 
     // ROTAS API
-    router::route('GET','/api/products/product/:number',"heartwood\\api\\controllers\\product");
+    router::route(
+        'GET',
+        '/api/products/product/:number',
+        "heartwood\\api\\controllers\\product",
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+    );
 
     exit(router::http_response_code(404));
 ?>
