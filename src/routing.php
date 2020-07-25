f93e9e9789c775666c30e13902a1c39a3f9347be
Douglas Gonçalves <douglas.goncalves@grupofolha.com.br>
2020-07-25 11:34:12 -0300

    Novo data para insert e update

--- src/routing.php
+++ src/routing.php
@@ -13,23 +13,33 @@
     // ROTAS ADMIN
     router::route('GET','/',"heartwood\\admin\\controllers\\dashboard");
 
-    router::route('GET','/admin/permissions/profile',"permission\\admin\\controllers\\profileList");
-    router::route('POST','/admin/permissions/profile',"permission\\admin\\controllers\\profileList");
-    router::route('GET','/admin/permissions/profile/new',"permission\\admin\\controllers\\profile");
+    router::route('GET','/admin/permissions/profile',"permission\\admin\\controllers\\profile");
+    router::route('POST','/admin/permissions/profile',"permission\\admin\\controllers\\profile");
+    router::route('GET','/admin/permissions/profileNew',"permission\\admin\\controllers\\profileNew");
+    router::route('POST','/admin/permissions/profileNew',"permission\\admin\\controllers\\profileNew");
     router::route('GET','/admin/permissions/profile/:number',"permission\\admin\\controllers\\profileUpdate");
     router::route('POST','/admin/permissions/profile/:number',"permission\\admin\\controllers\\profileUpdate");
 
-    router::route('GET','/admin/permissions/action',"permission\\admin\\controllers\\actionList");
-    router::route('GET','/admin/permissions/action/new',"permission\\admin\\controllers\\action");
+    router::route('GET','/admin/permissions/action',"permission\\admin\\controllers\\action");
+    router::route('POST','/admin/permissions/action',"permission\\admin\\controllers\\action");
+    router::route('GET','/admin/permissions/actionNew',"permission\\admin\\controllers\\actionNew");
+    router::route('POST','/admin/permissions/actionNew',"permission\\admin\\controllers\\actionNew");
     router::route('GET','/admin/permissions/action/:number',"permission\\admin\\controllers\\actionUpdate");
+    router::route('POST','/admin/permissions/action/:number',"permission\\admin\\controllers\\actionUpdate");
 
-    router::route('GET','/admin/permissions/permission',"permission\\admin\\controllers\\permissionList");
-    router::route('GET','/admin/permissions/permission/new',"permission\\admin\\controllers\\permission");
+    router::route('GET','/admin/permissions/permission',"permission\\admin\\controllers\\permission");
+    router::route('POST','/admin/permissions/permission',"permission\\admin\\controllers\\permission");
+    router::route('GET','/admin/permissions/permissionNew',"permission\\admin\\controllers\\permissionNew");
+    router::route('POST','/admin/permissions/permissionNew',"permission\\admin\\controllers\\permissionNew");
     router::route('GET','/admin/permissions/permission/:number',"permission\\admin\\controllers\\permissionUpdate");
+    router::route('POST','/admin/permissions/permission/:number',"permission\\admin\\controllers\\permissionUpdate");
 
-    router::route('GET','/admin/permissions/area',"permission\\admin\\controllers\\areaList");
-    router::route('GET','/admin/permissions/area/new',"permission\\admin\\controllers\\area");
+    router::route('GET','/admin/permissions/area',"permission\\admin\\controllers\\area");
+    router::route('POST','/admin/permissions/area',"permission\\admin\\controllers\\area");
+    router::route('GET','/admin/permissions/areaNew',"permission\\admin\\controllers\\areaNew");
+    router::route('POST','/admin/permissions/areaNew',"permission\\admin\\controllers\\areaNew");
     router::route('GET','/admin/permissions/area/:number',"permission\\admin\\controllers\\areaUpdate");
+    router::route('POST','/admin/permissions/area/:number',"permission\\admin\\controllers\\areaUpdate");
 
     // ROTAS API
     router::route(
