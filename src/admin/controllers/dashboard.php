1ae11c1cea09521efef99b281b712a6497578fab
Douglas Gonçalves <douglas.goncalves@grupofolha.com.br>
2020-10-31 13:03:05 -0300

    Inicio do desenvolvimento do departamento de compras

--- src/admin/controllers/dashboard.php
+++ src/admin/controllers/dashboard.php
@@ -3,13 +3,14 @@
     namespace heartwood\admin\controllers;
 
     use heartwood\admin\controllers\heartwood;
+    use standard_xml\admin\controllers\standard;
 
     class dashboard extends heartwood
     {
         const _LOCAL = __DIR__;
 
         /**
-         * Fun��o a ser executada no contexto da action
+         * Função a ser executada no contexto da action
          *
          * @param array $info
          * @return void
@@ -17,6 +18,29 @@
         public function main(array $info)
         {
             self::setLayout(self::getHeartwoodLayouts().'/cooladmin1.phtml');
+
+            $xml = array(
+                'age' => 52,
+                'db' => array(
+                    'host'   => array(
+                        'schema' => 'heartwood',
+                        'name'   => 'localhost',
+                        'port'   => 3316
+                    ),
+                    'user'  => 'douggonsouza',
+                    'password' => 'Ds@468677'
+                ),
+                'firstname' => 'Douglas',
+                'lastname'  => 'Souza'
+            );
+
+            standard::load(standard::MIME_XML);
+            // $content = standard::getFile()->start('config');
+            // standard::getFile()->addElement('document');
+            // standard::getFile()->addElement('schema','heartwood', null, standard::getFIle()->getElement('document')->item(0));
+            standard::getFile()->ofArray($xml, 'document');
+            $content = standard::content();
+
             return $this->view();
         }
     }
